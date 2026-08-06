df616545467e47e87fef60a6482c7a6c74617e3d
Alex Hodukov <alex.hodukov@gmail.com>
2026-08-06 04:23:45 +0300

    Created Spring Boot project

---
+++ src/test/java/com/clinicpulse/tracker/PatientVisitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicpulse.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientVisitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
