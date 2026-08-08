e8b58aec2768499d6dfae0a98e11cd01d69b9af5
Alex Hodukov <alex.hodukov@gmail.com>
2026-08-08 04:10:47 +0300

    Added service annotation, fixed unused imports

--- src/main/java/com/clinicpulse/tracker/service/VisitService.java
+++ src/main/java/com/clinicpulse/tracker/service/VisitService.java
@@ -2,7 +2,6 @@ package com.clinicpulse.tracker.service;
 
 import com.clinicpulse.tracker.dto.CreateVisitRequest;
 import com.clinicpulse.tracker.dto.CreateVisitResponse;
-import com.clinicpulse.tracker.entity.Patient;
 import com.clinicpulse.tracker.entity.Visit;
 import com.clinicpulse.tracker.exception.DoctorNotFoundException;
 import com.clinicpulse.tracker.exception.InvalidVisitIntervalException;
@@ -11,13 +10,14 @@ import com.clinicpulse.tracker.exception.VisitOverlapException;
 import com.clinicpulse.tracker.repository.DoctorRepository;
 import com.clinicpulse.tracker.repository.PatientRepository;
 import com.clinicpulse.tracker.repository.VisitRepository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.util.TimeZone;
 
+@Service
 public class VisitService {
 
     private final PatientRepository patientRepository;
